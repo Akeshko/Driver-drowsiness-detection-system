eb2ef527f84f2573785025e1bc295352113e2dcf
Akeshko <74096430+Akeshko@users.noreply.github.com>
2022-04-24 18:05:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ st.title("Driver Drowniness System")
 
 run = st.checkbox('Run')
 FRAME_WINDOW = st.image([])
-camera = cv2.VideoCapture(-1)
+camera = cv2.VideoCapture("videoplayback.mp4")
 
 hog_face_detector = dlib.get_frontal_face_detector()
 face_cas=cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
